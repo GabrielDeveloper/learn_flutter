7f9b25d86064f09180251052e668625fdaa0c4a5
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2018-12-28 16:40:07 -0200

    Added snackbar

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -21,7 +21,8 @@ class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
-
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -84,22 +85,7 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               padding: EdgeInsets.only(top: 10.0),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
-                    ),
-                  onChanged: (checked){
-                      setState(() {
-                        _toDoList[index]["ok"] = checked;
-                        _saveData();
-                      });
-                  },
-
-                );
-              },
+              itemBuilder: buildItem
             ),
           )
         ],
@@ -107,8 +93,59 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget buildItem (context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+          title: Text(_toDoList[index]["title"]),
+          value: _toDoList[index]["ok"],
+          secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+          ),
+          onChanged: (checked){
+            setState(() {
+              _toDoList[index]["ok"] = checked;
+              _saveData();
+            });
+          }),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
 
-  Future<File> _getFile() async {
+          _saveData();
+
+          final snack = SnackBar(
+              content: Text("Tarefa: \"${_lastRemoved["title"]}\" removida!"),
+              action: SnackBarAction(
+                  label: "Desfazer",
+                  onPressed: () {
+                    setState(() {
+                      _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                      _saveData();
+                    });
+                  }
+              ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+
+        });
+      },
+    );
+  }
+
+   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
