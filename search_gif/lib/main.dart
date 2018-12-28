5c69de77bfd454dc12072bb4c440ba82f78ce8d9
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2018-12-28 17:46:33 -0200

    Create gif app

---
+++ search_gif/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:search_gif/ui/HomePage.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white
+    ),
+  ));
+}
